dc9c2e1c1b31f0d7575319124213a47133d9b82d
anton-okex <77434555+anton-okex@users.noreply.github.com>
2021-04-13 19:49:06 +0300

    Update REST_examples.py

--- okex-python-sdk-api-v5/REST_examples.py
+++ okex-python-sdk-api-v5/REST_examples.py
@@ -24,100 +24,100 @@ if __name__ == '__main__':
     result = accountAPI.get_account('BTC')
     # get positions/получить позиции счета (BTC-USD, фьючерс)
     result = accountAPI.get_positions('FUTURES', 'BTC-USD-210402')
-    # Get Account Configuration/конфигурация аккаунта
+    # get account configuration/конфигурация аккаунта
     result = accountAPI.get_account_config()
-    # Get Position mode/получить режим исполнения позиции
+    # get position mode/получить режим исполнения позиции
     result = accountAPI.get_position_mode('long_short_mode')
-    # Set Leverage/установка маржи кредитного плеча 
+    # set leverage/установка маржи кредитного плеча 
     result = accountAPI.set_leverage(instId='BTC-USD-210402', lever='10', mgnMode='cross')
-    # Get Maximum Tradable Size For Instrument/получить максимально возможный размер для выбранного инструмента (фьючерс)  
+    # get maximum tradable size for instrument/получить максимально возможный размер для выбранного инструмента (фьючерс)  
     result = accountAPI.get_maximum_trade_size('BTC-USDT-210402', 'cross', 'USDT')
-    # Get Maximum Available Tradable Amount/получить максимально возможную сумму для фьючерсной сделки 
+    # get maximum available tradable amount/получить максимально возможную сумму для фьючерсной сделки 
     result = accountAPI.get_max_avail_size('BTC-USDT-210402', 'isolated', 'BTC')
-    # Increase/Decrease margin/Увеличение/сокращение маржи
+    # increase/decrease margin/Увеличение/сокращение маржи
     result = accountAPI.Adjustment_margin('BTC-USDT-210409', 'long', 'add', '100')
-    # Get Leverage/получить маржу кредитного плеча  
+    # get leverage/получить маржу кредитного плеча  
     result = accountAPI.get_leverage('BTC-USDT-210409', 'isolated')
-    # Get the maximum loan of isolated MARGIN/получить максимальную ссуду изолированного кредитного плеча
+    # get the maximum loan of isolated MARGIN/получить максимальную ссуду изолированного кредитного плеча
     result = accountAPI.get_max_load('BTC-USDT', 'cross', 'BTC')
-    # Get Fee Rates/получить размер комиссий (фьючерс)
+    # get fee rates/получить размер комиссий (фьючерс)
     result = accountAPI.get_fee_rates('FUTURES', '', category='1')
-    # Get interest-accrued/получить начисленные проценты (BTC-USDT)
+    # get interest-accrued/получить начисленные проценты (BTC-USDT)
     result = accountAPI.get_interest_accrued('BTC-USDT', 'BTC', 'isolated', '', '', '10')
-    # Get Maximum Withdrawals/установить максимальный размер выводoв
+    # get maximum withdrawals/установить максимальный размер выводoв
     result = accountAPI.get_max_withdrawal('')
 
     # funding api/основной аккаунт
     fundingAPI = Funding.FundingAPI(api_key, secret_key, passphrase, False, flag)
-    # Get Deposit Address/получить адрес для депозита
+    # get deposit address/получить адрес для депозита
     result = fundingAPI.get_deposit_address('')
-    # Get Balance/ получить баланс основного счета (в BTC)
+    # get balance/ получить баланс основного счета (в BTC)
     result = fundingAPI.get_balances('BTC')
-    # Funds Transfer/сделать трансфер средств на субсчет
+    # funds transfer/сделать трансфер средств на субсчет
     result = fundingAPI.funds_transfer(ccy='', amt='', type='1', froms="", to="",subAcct='')
-    # Withdrawal/сделать вывод средств (USDT)
+    # withdrawal/сделать вывод средств (USDT)
     result = fundingAPI.coin_withdraw('usdt', '2', '3', '', '', '0')
-    # Get Deposit History/получить историю депозитов
+    # get deposit history/получить историю депозитов
     result = fundingAPI.get_deposit_history()
-    # Get Withdrawal History/получить историю выводов
+    # get withdrawal history/получить историю выводов
     result = fundingAPI.get_withdrawal_history()
-    # Get Currencies/получить id валюты
+    # get currencies/получить id валюты
     result = fundingAPI.get_currency()
-    # PiggyBank Purchase/Redemption/получить выплату/приобрести в OKEx PiggyBank
+    # PiggyBank purchase/redemption/получить выплату/приобрести в OKEx PiggyBank
     result = fundingAPI.purchase_redempt('BTC', '1', 'purchase')
 
     # market api/рыночный API
     marketAPI = Market.MarketAPI(api_key, secret_key, passphrase, False, flag)
-    # Get Tickers/получить ВСЕ тикеры валют на спот рынке
+    # get tickers/получить ВСЕ тикеры валют на спот рынке
     result = marketAPI.get_tickers('SPOT')
-    # Get Ticker/получить тикер пары (BTC-USDT)
+    # get ticker/получить тикер пары (BTC-USDT)
     result = marketAPI.get_ticker('BTC-USDT')
-    # Get Index Tickers/получить индексный тикер в паре (BTC-USDT)
+    # get index tickers/получить индексный тикер в паре (BTC-USDT)
     result = marketAPI.get_index_ticker('BTC', 'BTC-USD')
-    # Get Order Book/получить информацию по OrderBook (BTC-USDT)
+    # get order book/получить информацию по OrderBook (BTC-USDT)
     result = marketAPI.get_orderbook('BTC-USDT-210402', '400')
-    # Get Candlesticks/получить информацию по свечам (BTC-USDT)
+    # get candlesticks/получить информацию по свечам (BTC-USDT)
     result = marketAPI.get_candlesticks('BTC-USDT-210924', bar='1m')
-    # Get Candlesticks History（top currencies in real-trading only/получить историю по свечам (недоступно в демо-режиме)
+    # get candlesticks history（top currencies in real-trading only/получить историю по свечам (недоступно в демо-режиме)
     result = marketAPI.get_history_candlesticks('BTC-USDT')
-    # Get Index Candlesticks/получить информацию по свечам (индекс)
+    # get index candlesticks/получить информацию по свечам (индекс)
     result = marketAPI.get_index_candlesticks('BTC-USDT')
-    # Get Trades/получить информацию по сделкам (BTC-USDT, 400)
+    # get trades/получить информацию по сделкам (BTC-USDT, 400)
     result = marketAPI.get_trades('BTC-USDT', '400')
 
     # public api/публичный API
     publicAPI = Public.PublicAPI(api_key, secret_key, passphrase, False, flag)
-    # Get instrument/получить информацию по трейдинговому инструменту (фьючерс, BTC-USDT)
+    # get instrument/получить информацию по трейдинговому инструменту (фьючерс, BTC-USDT)
     result = publicAPI.get_instruments('FUTURES', 'BTC-USDT')
-    # Get Delivery/Exercise History/получить историю по инструменту/паре (фьючерс, BTC-USDT)
+    # get delivery/exercise history/получить историю по инструменту/паре (фьючерс, BTC-USDT)
     result = publicAPI.get_deliver_history('FUTURES', 'BTC-USD')
-    # Get Open Interest/получить информацию по открытым позициям (своп)
+    # get open interest/получить информацию по открытым позициям (своп)
     result = publicAPI.get_open_interest('SWAP')
-    # Get Funding Rate/получить информацию по текущей ставке (BTC-USDT, своп)
+    # get funding rate/получить информацию по текущей ставке (BTC-USDT, своп)
     result = publicAPI.get_funding_rate('BTC-USD-SWAP')
-    # Get Funding Rate History/получить информацию по истории ставки (BTC-USDT, своп)
+    # get funding rate history/получить информацию по истории ставки (BTC-USDT, своп)
     result = publicAPI.funding_rate_history('BTC-USD-SWAP')
-    # Get Limit Price/получить цену лимита (BTC-USDT)
+    # get limit price/получить цену лимита (BTC-USDT)
     result = publicAPI.get_price_limit('BTC-USD-210402')
-    # Get Option Market Data/получить рыночные данные по опционам (BTC-USD)
+    # get option market data/получить рыночные данные по опционам (BTC-USD)
     result = publicAPI.get_opt_summary('BTC-USD')
-    # Get Estimated Delivery/Excercise Price/получить цену исполнения (ETH-USD)
+    # get estimated delivery/excercise price/получить цену исполнения (ETH-USD)
     result = publicAPI.get_estimated_price('ETH-USD-210326')
-    # Get Discount Rate And Interest-Free Quota/получить информацию по ставке и беспроцентной квоте
+    # get discount rate & interest-free quota/получить информацию по ставке и беспроцентной квоте
     result = publicAPI.discount_interest_free_quota('')
-    # Get System Time/системное время
+    # get system time/системное время
     result = publicAPI.get_system_time()
-    # Get Liquidation Orders/получить информацию по ликвидационным ордерам (фьючерс, BTC-USDT)
+    # get liquidation orders/получить информацию по ликвидационным ордерам (фьючерс, BTC-USDT)
     result = publicAPI.get_liquidation_orders('FUTURES', uly='BTC-USDT', alias='next_quarter', state='filled')
-    # Get Mark Price/получить информацию по цене отметки инструмента (фьючерс)
+    # get mark price/получить информацию по цене отметки инструмента (фьючерс)
     result = publicAPI.get_mark_price('FUTURES')
 
     # trade api/торговый API
     tradeAPI = Trade.TradeAPI(api_key, secret_key, passphrase, False, flag)
-    # Place Order/поставить ордер 
+    # place order/поставить ордер 
     result = tradeAPI.place_order(instId='BTC-USDT-210326', tdMode='cross', side='sell', posSide='short',
                                     ordType='market', sz='100')
-    # Place Multiple Orders/поставить несколько ордеров
+    # place multiple orders/поставить несколько ордеров
     result = tradeAPI.place_multiple_orders([
           {'instId': 'BTC-USD-210402', 'tdMode': 'isolated', 'side': 'buy', 'ordType': 'limit', 'sz': '1', 'px': '17400',
            'posSide': 'long',
@@ -127,57 +127,57 @@ if __name__ == '__main__':
            'clOrdId': 'a12344444', 'tag': 'test1211'}
       ])
 
-    # Cancel Order/oтменить ордер
+    # cancel order/oтменить ордер
     result = tradeAPI.cancel_order('BTC-USD-201225', '257164323454332928')
-    # Cancel Multiple Orders/oтменить несколько ордеров
+    # cancel multiple orders/oтменить несколько ордеров
     result = tradeAPI.cancel_multiple_orders([
           {"instId": "BTC-USD-210402", "ordId": "297389358169071616"},
           {"instId": "BTC-USD-210409", "ordId": "297389358169071617"}
       ])
 
-    # Amend Order/изменить ордер
+    # amend order/изменить ордер
     result = tradeAPI.amend_order()
-    #   Amend Multiple Orders/изменить несколько ордеров
+    #   amend multiple orders/изменить несколько ордеров
     result = tradeAPI.amend_multiple_orders(
           [{'instId': 'BTC-USD-201225', 'cxlOnFail': 'false', 'ordId': '257551616434384896', 'newPx': '17880'},
            {'instId': 'BTC-USD-201225', 'cxlOnFail': 'false', 'ordId': '257551616652488704', 'newPx': '17882'}
            ])
 
-    # Close Positions/закрыть позицию
+    # close positions/закрыть позицию
     result = tradeAPI.close_positions('BTC-USDT-210409', 'isolated', 'long', '')
-    # Get Order Details/получить информацию по конкретному ордеру
+    # get order details/получить информацию по конкретному ордеру
     result = tradeAPI.get_orders('BTC-USD-201225', '257173039968825345')
-    # Get Order List/получить список ордеров
+    # get order list/получить список ордеров
     result = tradeAPI.get_order_list()
-    # Get Order History (last 7 days/получить историю ордеров (последние 7 дней, фьючерс)
+    # get order history (last 7 days)/получить историю ордеров (последние 7 дней, фьючерс)
     result = tradeAPI.get_orders_history('FUTURES')
-    # Get Order History (last 3 months)/получить историю ордеров (последние 3 месяца, фьючерс)
+    # get order history (last 3 months)/получить историю ордеров (последние 3 месяца, фьючерс)
     result = tradeAPI.orders_history_archive('FUTURES')
-    # Get Transaction Details/получить историю транзакций
+    # get transaction details/получить историю транзакций
     result = tradeAPI.get_fills()
-    # Place Algo Order/пример постановки алгоритмического ордера
+    # place algo order/пример постановки алгоритмического ордера
     result = tradeAPI.place_algo_order('BTC-USDT-210409', 'isolated', 'buy', ordType='conditional',
                                          sz='100',posSide='long', tpTriggerPx='60000', tpOrdPx='59999')
-    # Cancel Algo Order/отменить алгоритмический ордер
+    # cancel algo order/отменить алгоритмический ордер
     result = tradeAPI.cancel_algo_order([{'algoId': '297394002194735104', 'instId': 'BTC-USDT-210409'}])
-    # Get Algo Order List/получить список алгоритмических ордеров
+    # get algo order List/получить список алгоритмических ордеров
     result = tradeAPI.order_algos_list('conditional', instType='FUTURES')
-    # Get Algo Order History/получить историю алгоритмических ордеров
+    # get algo order History/получить историю алгоритмических ордеров
     result = tradeAPI.order_algos_history('conditional', 'canceled', instType='FUTURES')
 
     # API subAccount/API субсчетoв
     subAccountAPI = SubAccount.SubAccountAPI(api_key, secret_key, passphrase, False, flag)
-    # Query detailed balance info of Trading Account of a sub-account via the master account/получить подробную информацию о балансе субсчета через основной счет
+    # query detailed balance info of trading account of a sub-account via the master account/получить подробную информацию о балансе субсчета через основной счет
     result = subAccountAPI.balances(subAcct='')
-    # History of sub-account transfer(applies to master accounts only)/получить историю трансферов всех субсчетов (работает только с основного счета)
+    # history of sub-account transfer(applies to master accounts only)/получить историю трансферов всех субсчетов (работает только с основного счета)
     result = subAccountAPI.bills()
-    # Delete the APIkey of sub-accounts (applies to master accounts only)/удалить API ключ субсчета (работает только с основного счета)
+    # delete the APIkey of sub-accounts (applies to master accounts only)/удалить API ключ субсчета (работает только с основного счета)
     result = subAccountAPI.delete(pwd='', subAcct='', apiKey='')
-    # Reset the APIkey of a sub-account(applies to master accounts only)/сбросить API ключ субсчета (работает только с основного счета)
+    # reset the APIkey of a sub-account(applies to master accounts only)/сбросить API ключ субсчета (работает только с основного счета)
     result = subAccountAPI.reset(pwd='', subAcct='', label='', apiKey='', perm='')
-    # Create an APIkey for a sub-account(applies to master accounts only)/создать API ключ субсчета (работает только с основного счета)
+    # create an APIkey for a sub-account(applies to master accounts only)/создать API ключ субсчета (работает только с основного счета)
     result = subAccountAPI.create(pwd='123456', subAcct='', label='', Passphrase='')
-    # View sub-account list(applies to master accounts only)/получить список субсчетов (работает только с основного счета)
+    # view sub-account list(applies to master accounts only)/получить список субсчетов (работает только с основного счета)
     result = subAccountAPI.view_list()
     # manage the transfers between sub-accounts(applies to master accounts only)/управление трансферами между субсчетами (работает только с основного счета)
     result = subAccountAPI.control_transfer(ccy='', amt='', froms='', to='', fromSubAccount='', toSubAccount='')
